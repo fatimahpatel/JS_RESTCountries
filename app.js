a2cc5bf2e5bcc1fa4098eeb80a4767d2de3c1419
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-18 15:30:52 +0000

    psuedocode for first task

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ const getCountryByName = (countryName) => {
 }
 
 getCountryByName("uk");
+
+
+//  You'll need to access specific properties within the returned object, such as name. 
+//need to do await fetch for country name 
+//convert this data to json
+//need to access the section
+//insert the information into the section? - might need to use div?
+
+
+
+//This will also involve you creating a variable associated with the section 
+//created earlier and making use of the createElement() and .append() methods
+
+
