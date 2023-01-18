acacdd67039e156cda60baa86ac08d3d7276014a
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-18 15:06:11 +0000

    Started creating fetch request

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hi!");
+
+const getCountryByName = (countryName) => {
+    const response = fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => response.json()) //takes the response and puts it into JSON format
+    console.log(response);
+}
+
+getCountryByName("germany");
