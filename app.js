0f548044bf3334d2078ec8911b04de02b9ce3408
Fatimah Patel <fatimahpatel10@gmail.com>
2023-01-18 19:16:46 +0000

    Created new function which adds country name and population to section using p tag

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
 console.log("Hi!");
 
+//function for inputting country name and population to p tag
+
+const addCountryNameAndPopulation = (jsonData) =>{
+    jsonData.forEach(country => { //looping through each country
+        const accessSection = document.querySelector("section"); //selecting the section
+
+        //adding name and population to textContainer
+        const textContainer = document.createElement("p");
+        textContainer.textContent = country.name.common + " " + country.population;
+        accessSection.appendChild(textContainer)});
+}
 
 //get country by name
 const getCountryByName = async (countryName) => {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    const jsonData = await response.json(); //takes the response and puts it into JSON format
-    const accessSection = document.querySelector("section"); //selecting the section
-    console.log(jsonData);
-    jsonData.forEach(country => { //looping through each country
+    const jsonDataForOneCountry = await response.json(); //takes the response and puts it into JSON format
+    console.log(jsonDataForOneCountry);
+    addCountryNameAndPopulation(jsonDataForOneCountry);
 
-        //adding name and population to textContainer
-        const textContainer = document.createElement("p");
-        textContainer.textContent = country.name.common + country.population;
-        accessSection.appendChild(textContainer);
-
-    })
+    // jsonData.forEach(country => { //looping through each country
+        // //adding name and population to textContainer
+        // const textContainer = document.createElement("p");
+        // textContainer.textContent = country.name.common + country.population;
+        // accessSection.appendChild(textContainer);
+    // })
 }
-getCountryByName("america");
+
+//new function to display name and population of all countries
+// const getAllCountries = async () =>{
+//     const response = await fetch("https://restcountries.com/v3.1");
+//     const jsonData = await response.json();
+// }
+
+
+
+
+getCountryByName("germany");
+// getAllCountries();
 
 
 
